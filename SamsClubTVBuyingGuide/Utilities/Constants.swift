9fe4341de5e4b652cc8da2e7865322e1761b40f1
daver234 <dave.rothschild@gmail.com>
2017-09-26 15:01:00 -0700

    - Update XIB for UI - Add constants

---
+++ SamsClubTVBuyingGuide/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  SamsClubTVBuyingGuide
+//
+//  Created by David Rothschild on 9/26/17.
+//  Copyright © 2017 Dave Rothschild. All rights reserved.
+//
+
+import Foundation
