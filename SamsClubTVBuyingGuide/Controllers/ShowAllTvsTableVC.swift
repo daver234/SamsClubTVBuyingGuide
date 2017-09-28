2f5812892d60a71dd6d2fa4ea5ef90510d78fe8f
daver234 <dave.rothschild@gmail.com>
2017-09-27 22:28:24 -0700

    - move table view data fetching to separate function - code clean up

--- SamsClubTVBuyingGuide/Controllers/ShowAllTvsTableVC.swift
+++ SamsClubTVBuyingGuide/Controllers/ShowAllTvsTableVC.swift
@@ -59,10 +59,10 @@ extension ShowAllTvsTableVC {
         return NetworkManager.instance.totalProductsRetrieved
     }
     
-    /// Use Apple and Kingfisher prefetching to download images ahead of time to improve the user experience.
+    /// Here we prefetch the images for the tableview using Kingfisher.
+    /// And, prefetch more data if the user is getting to the end of the tableview.
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         /// First get the images
-        // print("&&&& just before guard, subscript to All products is: ", (currentPage - 1))
         let currentPage = NetworkManager.instance.numberOfPagesRetrieved - 1
         guard let product = NetworkManager.instance.allProducts[currentPage].products  else {
             return
@@ -70,48 +70,37 @@ extension ShowAllTvsTableVC {
         let urls = product.flatMap { $0.productImage }
         ImagePrefetcher(urls: urls).start()
         
-        
-        ////// Testing here
-        /// Then get more data
+        /// This section is for prefetching more data before the user gets to the end of the tableview with the current data
+        /// First, build an array of all the upcoming rows.
         let upcomingRows = indexPaths.map { $0.row }
+        print("&&& here is upcoming rows", upcomingRows)
         
+        /// Check to see what the max upcoming row number is
         if let maxIndex = upcomingRows.max() {
+            fetchMoreData(maxIndex: maxIndex, currentPage: currentPage)
+        }
+    }
+    
+    /// Fetch more data if the user is getting to the end of the tableview
+    func fetchMoreData(maxIndex: Int, currentPage: Int) {
+        /// Here take maxIndex and add 5 to give some buffer for how soon we should get more data from the server.
+        /// This will improve the user experience so no delay is perceived when scrolling the table.
+        /// All of this determines the nextPage to get
+        let nextPage: Int = ( (maxIndex + 5) / PAGE_SIZE)
+        print("&&& FIRST nextPage", nextPage)
+        print("&&&&&& maxIndex", maxIndex)
+        
+        /// If the user is getting to the end of the table, nextPage will be greater than current page.
+        /// This will then trigger a API call to get more data from the server and return, ideally, before
+        /// the user gets to the end of the table.
+        if nextPage > currentPage {
+            print("&&& in nextPage >", nextPage)
+            print("&&& Here is current page", currentPage)
+            print("&&& here is maxIndex", maxIndex)
+            print("done ShowAll -------------------")
             
-            // let nextPage: Int = Int(ceil(Double(maxIndex) / Double(PAGE_SIZE)))
-            
-            /// The maxIndex in the highest indexPath.row that is upcoming.
-            /// Adding to maxIndex causes the API call to happen 5 cells sooner that where the spinner will show.
-            /// This allows for time to get the data and reduce the amount of time the user sees the spinner.
-            /// So in general, this should improve the user experience.
-            let nextPage: Int = ( (maxIndex + 5) / PAGE_SIZE)
-            print("&&& FIRST nextPage", nextPage)
-//            print("Here is current page", currentPage)
-            print("&&&&&& maxIndex", maxIndex)
-//            print("here is upcoming rows", upcomingRows)
-            
-            if nextPage > currentPage {
-                print("&&& in nextPage >", nextPage)
-                print("&&& Here is current page", currentPage)
-                print("&&& here is maxIndex", maxIndex)
-                print("&&& here is upcoming rows", upcomingRows)
-                print("done ShowAll -------------------")
-                // Your function, which attempts to load respective page from the local database
-                //loadLocalData(page: nextPage)
-                
-                // Check to see if we are done loading data.  Otherwise continue and make another API call
-//                guard !NetworkManager.instance.isEndOfData else {
-//                    print("&&& in isEndOfData")
-//                    return
-//                }
-                
-//                if NetworkManager.instance.isEndOfData {
-//                    self.tableView.reload
-//                }
-                
-                // Your function, which makes a network request to fetch the respective page of data from the network
-                //startLoadingDataFromNetwork(page: nextPage
-                let _ = "Here"
-                NetworkManager.instance.getProductsForPage(pageNumber: nextPage, pageSize: PAGE_SIZE) { (response) in
+            let _ = "Here"
+            NetworkManager.instance.getProductsForPage(pageNumber: nextPage, pageSize: PAGE_SIZE) { (response) in
                 if response {
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
@@ -120,7 +109,6 @@ extension ShowAllTvsTableVC {
                     print("&&& loaded all the data.  Done")
                 }
                 // self.currentPage = nextPage
-                }
             }
         }
     }
@@ -130,19 +118,8 @@ extension ShowAllTvsTableVC {
             return UITableViewCell()
         }
         
-        // Check to see if we are done loading data.  Otherwise continue and make another API call
-//        guard !NetworkManager.instance.isEndOfData else {
-//            print("&&& in isEndOfData")
-//            return UITableViewCell()
-//        }
         print("&&&& indexPath.row is: ", indexPath.row)
-        // print("&&& (currentPage): ", currentPage)
-        /// If we at PAGE_SIZE, go get the next page
-//        if indexPath.row == PAGE_SIZE {
-//            DispatchQueue.main.async {
-//                tableView.reloadData()
-//            }
-//        }
+
         let pageToGet = indexPath.row / PAGE_SIZE
         let productToGet = indexPath.row % PAGE_SIZE
         guard let product = NetworkManager.instance.allProducts[pageToGet].products?[productToGet]  else {
@@ -153,47 +130,20 @@ extension ShowAllTvsTableVC {
         return cell
     }
     
-        
-        
-    /// might try this for spinner and data loading.....https://stackoverflow.com/questions/32425466/load-more-after-coming-to-bottom-of-uitableview
-    /// Add spinner at bottom of table view when loading more data from the API call
+    /// This displays the "end of data" footer view when the user has scolled to the end of the available data.
+    /// Because of the prefetching of data above, the user does not need to see a spinner when more data is loading.
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let lastSectionIndex = tableView.numberOfSections - 1
         let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
         if indexPath.section ==  lastSectionIndex && indexPath.row == lastRowIndex {
             
             tableFooterView.isHidden = false
-//            let spinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
-//            spinner.startAnimating()
-//            spinner.frame = CGRect(x: CGFloat(0), y: CGFloat(0), width: tableView.bounds.width, height: CGFloat(44))
-//
-//            self.tableView.tableFooterView = spinner
-//            self.tableView.tableFooterView?.isHidden = false
-            
-            // need to set spinner to nil after getting data
         }
     }
     
     /// might try this for spinner and data loading.....https://stackoverflow.com/questions/32425466/load-more-after-coming-to-bottom-of-uitableview
     // https://stackoverflow.com/questions/20269474/uitableview-load-more-when-scrolling-to-bottom-like-facebook-application
-//    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-//
-//        // UITableView only moves in one direction, y axis
-//        let currentOffset = scrollView.contentOffset.y
-//        let maximumOffset = scrollView.contentSize.height - scrollView.frame.size.height
-//
-//        // Change 10.0 to adjust the distance from bottom
-//        if maximumOffset - currentOffset <= 10.0 {
-//            // self.loadMore()
-//            /// Get more products
-//            NetworkManager.instance.getProductsForPage(pageNumber: 1, pageSize: PAGE_SIZE) { (response) in
-//                DispatchQueue.main.async {
-//                    self.tableView.reloadData()
-//                }
-//            }
-//        }
-//    }
-//
+
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
