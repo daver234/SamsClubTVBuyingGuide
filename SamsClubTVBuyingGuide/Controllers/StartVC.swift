3be5cc044873cbb35cb8008e680fd08093067217
daver234 <dave.rothschild@gmail.com>
2017-09-29 18:20:05 -0700

    - Implemented SwiftSpinner if API call is delayed - Update text

--- SamsClubTVBuyingGuide/Controllers/StartVC.swift
+++ SamsClubTVBuyingGuide/Controllers/StartVC.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import SafariServices
+import SwiftSpinner
 
 class StartVC: UIViewController, SFSafariViewControllerDelegate {
 
     // MARK: - Variables
     let reachability = Reachability()!
+    var isNetworkDone = false
     
     // MARK: - Functions
     override func viewDidLoad() {
@@ -20,7 +22,14 @@ class StartVC: UIViewController, SFSafariViewControllerDelegate {
         
         /// Connection available to get data from API
         /// By loading products here we have avoid a delay when the user goes to view the list of products in ShowAllTvsTableVC
-        NetworkManager.instance.getProductsForPage(pageNumber: STARTING_PAGE_NUMBER, pageSize: PAGE_SIZE) { (response) in
+        NetworkManager.instance.getProductsForPage(pageNumber: STARTING_PAGE_NUMBER, pageSize: PAGE_SIZE) { [weak self] (response) in
+            if response {
+                // guard let strongSelf = self else { return }
+                self?.isNetworkDone = true
+                DispatchQueue.main.async {
+                    SwiftSpinner.hide()
+                }
+            }
         }
         
         /// Set up mock data in case user looks at top rated
@@ -42,12 +51,32 @@ class StartVC: UIViewController, SFSafariViewControllerDelegate {
         NotificationCenter.default.removeObserver(self, name: .reachabilityChanged, object: reachability)
     }
     
+    // MARK: - IBActions
+    
     /// Open Safari View Controller to view Tom's Guide to TV Buying
     @IBAction func tomsGuidBtnPressed(_ sender: Any) {
         guard let url = URL(string: TOMS_GUIDE) else { return }
         let safariVC = SFSafariViewController(url: url)
         safariVC.delegate = self
         self.present(safariVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func showAllTvsBtnPressed(_ sender: Any) {
+        if isNetworkDone {
+            SwiftSpinner.hide()
+            performSegue(withIdentifier: SHOW_ALL_TVS, sender: nil)
+        } else {
+            SwiftSpinner.show("Every TV on its way...")
+            let when = DispatchTime.now() + 2 // change 2 to desired number of seconds
+            DispatchQueue.main.asyncAfter(deadline: when) { [weak self] in
+                SwiftSpinner.hide()
+                self?.performSegue(withIdentifier: SHOW_ALL_TVS, sender: nil)
+            }
+        }
+    }
+    
+    @IBAction func showTopRatedBtnPressed(_ sender: Any) {
+        SwiftSpinner.show("Just the top rated...")
     }
     
     /// When user taps Done button, Safari View Controller dismisses and returns to StartVC
