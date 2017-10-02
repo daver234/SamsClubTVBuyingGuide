75d1d946db8902f50b759c8a5cc6a522cc01b2f2
daver234 <dave.rothschild@gmail.com>
2017-10-01 21:53:11 -0700

    - More code clean up

--- SamsClubTVBuyingGuide/Utilities/Extensions.swift
+++ SamsClubTVBuyingGuide/Utilities/Extensions.swift
@@ -40,24 +40,3 @@ extension UIViewController {
         present(alert, animated: true, completion: nil)
     }
 }
-
-extension UIApplication {
-    static func topViewController(base: UIViewController? = UIApplication.shared.delegate?.window??.rootViewController) -> UIViewController? {
-        if let nav = base as? UINavigationController {
-            return topViewController(base: nav.visibleViewController)
-        }
-        
-        if let presented = base?.presentedViewController {
-            return topViewController(base: presented)
-        }
-        return base
-    }
-}
-
-class ErrorReporting {
-    static func showMessage(title: String, msg: String) {
-        let alert = UIAlertController(title: title, message: msg, preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
-        UIApplication.topViewController()?.present(alert, animated: true, completion: nil)
-    }
-}
